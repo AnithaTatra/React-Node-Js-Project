98e7199daabcdf7f9e9eaf2e5ce72d2bd91a5d89
AnithaTatra <102857170+AnithaTatra@users.noreply.github.com>
2022-06-20 11:16:02 +0530

    commit

---
+++ constants/constants.js
@@ -0,0 +1,11 @@
+module.exports.USER_STATUS = Object.freeze({
+    SUCCESS_STATUS           :"success",
+    FAILURE_STATUS           :"failure",
+    CREATED_SUCCESS          :"Signup successful",
+    NO_DATA                  :"No data Found",
+    PLEASE_SIGNUP            :"Please Signup",
+    CORRECT_USERNAME         :"PLease enter correct username",
+    LOGIN_SUCCESS            :"Login success",
+    LOGIN_FAILED             :"Login Failed",
+    LOGOUT_SUCCESS           :"Logout success",
+  });
